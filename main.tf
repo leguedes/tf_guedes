b5a078ed9a3880392c6b87cf755995a9c1115e4a
Leandro Guedes <139709968+leguedes@users.noreply.github.com>
2023-08-03 19:22:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_service_plan" "sp" {
   os_type             = "Linux"
 }
 
-resource "azurerm_linux_web_app" "wepapp" {
+resource "azurerm_linux_web_app" "webapp" {
   name                = var.app_service_name
   resource_group_name = azurerm_resource_group.leguedes.name
   location            = azurerm_service_plan.sp.location
