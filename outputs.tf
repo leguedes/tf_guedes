017aee1bcf7ae7a3b5b43ea7fb5b054a6fc21ba9
Leandro Guedes <139709968+leguedes@users.noreply.github.com>
2023-08-03 19:20:40 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "app_name" {
   description = "This is the unique name of the App Service that was created"
-  value       = azurerm_windows_web_app.wp.name
+  value       = azurerm_linux_web_app.webapp.name
 }
 
 output "app_url" {
   description = "This is the URL of the App Service that was created"
-  value       = "https://${azurerm_windows_web_app.wp.default_hostname}"
+  value       = "https://${azurerm_linux_web_app.webapp.default_hostname}"
 }
